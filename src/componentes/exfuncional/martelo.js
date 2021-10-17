cdec46429fb632e16b303b94a2536361c31594e1
Victor Emanuel <victoremanuel.mff@gmail.com>
2021-10-16 21:32:16 -0300

    Componentes

---
+++ src/componentes/exfuncional/martelo.js
@@ -0,0 +1,74 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View,ScrollView, Image, Modal, Button, TouchableHighlight} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Martelo(){
+
+    const [modalVisivel, setModalVisivel] = useState(false)
+
+    return(
+        <SafeAreaView style={styles.safe}>
+            <View style={styles.container}>
+                <Text style={styles.text}>Exercicio Martelo é bom faça</Text>
+                <View style={styles.centeredView}>
+                    <Modal 
+                    animationType='slide' 
+                    transparent={true} 
+                    visible={modalVisivel} 
+                    onRequestClose={() => {setModalVisivel(!modalVisivel)}}>
+                        <View style={styles.modalView}>
+                            <Image source={require('../../gifs/martelo.gif')}/>
+                            <Button title='Fechar' onPress={() => setModalVisivel(!modalVisivel)}/>
+                        </View>
+                    </Modal>
+                </View>
+                <Button title='Abrir' onPress={() => setModalVisivel(true)} />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#c4c4c4',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    safe:{
+      flex:1,
+    },
+    text:{
+      fontSize:42,
+      textAlign: 'left'
+    },
+    video: {
+      alignSelf: 'center',
+      width: 320,
+      height: 200,
+    },
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      margin: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5
+    },
+  })
